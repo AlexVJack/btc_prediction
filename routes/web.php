75beccc5f71150c25e2c881ef68241d6ac706bee
Alex <alexvladjack@gmail.com>
2021-06-01 11:24:36 +0300

    first simple example graph

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use ArielMejiaDev\LarapexCharts\LarapexChart;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('chart', function () {
+    $chart = (new LarapexChart)->lineChart()
+    ->setTitle('BTC price')
+    ->setSubtitle('Simple linear regression')
+    ->addData('Physical sales', [40, 93, 35, 42, 18, 82])
+    ->addData('Digital sales', [70, 29, 77, 28, 55, 45])
+    ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
+    
+    return view('chart', compact('chart'));
+}); 
